5dd744a3b5abbeb5e13753906c3887e917cddfd9
jqasim1 <59454388+jqasim1@users.noreply.github.com>
2021-03-22 20:03:03 -0500

    Add files via upload

---
+++ Login/Login/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+
+namespace Login
+{
+    class MainClass
+    {
+        static string username;
+        static string password;
+
+        public static void Main(string[] args)
+        {
+            Register();
+            Login();
+        }
+
+        public static void Register()
+        {
+            Console.WriteLine("Please create a username");
+            username = Console.ReadLine();
+            Console.WriteLine("Please create a password");
+            password = Console.ReadLine();
+            Console.WriteLine("Registration Complete!");
+            Console.WriteLine("-----------------------------------");
+
+        }
+
+        public static void Login()
+        {
+                Console.WriteLine("Please enter your username");
+                if (username == Console.ReadLine())
+                {
+                    Console.WriteLine("Please enter your password");
+                    if (password == Console.ReadLine())
+                    {
+                        Console.WriteLine("Login completed. Welcome back!");
+                    }
+                    else
+                    {
+                        Console.WriteLine("Wrong password");
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("Wrong username");
+                }
+        }
+    }
+}
